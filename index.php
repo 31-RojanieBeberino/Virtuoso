c532d3babbec70903aeb1a7c5c4927740493a10c
Rojanie <rojaniebeberino0206@gmail.com>
2021-02-02 20:59:15 +0800

    done

--- index.php
+++ index.php
@@ -16,7 +16,9 @@
 <body>
     <nav>
         <div class="nav-logo">
-            <h1>Vertuoso</h1>
+            <div class="virtuosoLogo">
+                <img src="img/AUE-AI2-02.png" alt="">
+            </div>
         </div>
         <div class="burger">
             <div class="l"></div>
@@ -33,7 +35,8 @@
     </nav>
     <div class="container1">
         <div class="mainHeading">
-            <h1>Save on Energy Costs with a FREE Hot Water Upgrades</h1>
+            <h2>Save on Energy Costs with a </h2>
+            <h1>FREE Hot Water Upgrades</h1>
             <br/>
             <div class="links">
                <div>
@@ -41,51 +44,46 @@
                     charge thanks to the Victorian Government’s Energy Upgrades Program.</p>
                 </div>
             </div>
+            <button class="btnMian">SECURE YOUR FREE HEAT PUMP</button>
         </div>
     </div>
     <div class="overView" id="container2">
        <div class="overviewContent"> 
-            <div>
+                <div>
+                    <h2>Victorian Energy Upgrades</h2>
+                </div>
                 <h1>Overview</h1>
-            </div>
-            <br>
-            <div class="overviewContent">
-                    <p>Victorian Energy Upgrades is an energy efficiency program designed by the Victorian Government to
-                    help businesses and households decrease their carbon footprint through incentive-driven renewable
-                    energy upgrades. Learn more about the program by watching this short clip.</p>
-            </div>
+                <p>Victorian Energy Upgrades is an energy efficiency program designed by the Victorian Government to
+                help businesses and households decrease their carbon footprint through incentive-driven renewable
+                energy upgrades. Learn more about the program by watching this short clip.</p>
+        </div>
+        <div class="overviewVid">
+            <iframe
+                src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
         </div>
     </div>
     <div class="about" id="project">
         <div class="content2">
             <div class="contentElement">
                 <h1>About the Rebates</h1>
-                <p>The amount of rebate homes and businesses can receive depends on the market activity and the
-                certificate price.</p>
+                <h2>The amount of rebate homes and businesses can receive depends on the market activity and the
+                certificate price.</h2>
+                <button class="btnLearmore">LEARN MORE</button>
             </div>
         </div>
     </div>
-    </div>
-    <div class="footerBanner">
-        <div class="footerHeader">
-            <h1 class="footerHead">Our passion </h1>
-            <br>
-            <p class="footerBody">Our passion is to help Australians become more energy independent. After a tumultuous 10 years of
-            constant power company price increases, black-outs and coal-fired power plant failures, we’re
-            seeing more Australians searching for a better solution
-        </p>
-        </div>
     </div>
     <div class="footer">
         <div class="footerContent">
             <div class="footer-brand">
                 <h1>Vertuoso</h1>
-                <div class="social-links">
-                    <i class="fab fa-facebook"></i>
-                    <i class="fab fa-instagram-square"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-youtube"></i>
-                </div>
+                    <h1 class="footerHead">Our passion </h1>
+                    <br>
+                    <p class="footerBody">Our passion is to help Australians become more energy independent. After a tumultuous 10 years of
+                    constant power company price increases, black-outs and coal-fired power plant failures, we’re
+                    seeing more Australians searching for a better solution
+                    </p>
             </div>
             <div class="footer-pages">
                 <h3>Pages</h3>
@@ -106,6 +104,12 @@
                     <li><a href="#">P.O. Box 2077 Grovedale East 3216</a></li>
                     <li><a href="#">1300 996 917</a></li>
                 </ul>
+                <div class="social-links">
+                    <i class="fab fa-facebook"></i>
+                    <i class="fab fa-instagram-square"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
             </div>
             <div class="footer-form">
                 <h3>Send your message here.</h3>
